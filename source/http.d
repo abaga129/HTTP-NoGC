682165c7a2fd9076c3b0ab57eae3f53873ea38b8
abaga129 <ethanepr@hotmail.com>
2018-05-23 21:46:26 -0500

    add Http Post method

--- source/http.d
+++ source/http.d
@@ -6,7 +6,7 @@ import utility;
 
 import core.stdc.stdio;
 import dplug.core.nogc;
-
+import core.stdc.stdlib;
 /// Class for performing nothrow @nogc synchronous HTTP requests in D 
 public static class Http(ResponseType)
 {
@@ -36,12 +36,17 @@ nothrow:
         SocketNoGC socket = mallocNew!SocketNoGC();
         socket.Connect(cast(string)server, port);
 
-        char[] header = concat(cast(const char*)"GET ", cast(const(char*))queryString);
+        char* contentLength = cast(char*)malloc(char.sizeof * 32);
+        sprintf(contentLength, "%d".ptr, contentBody.length);
+        char[] header = concat(cast(const char*)"POST ", cast(const(char*))queryString);
         header = concat(cast(const char*)header, cast(const char*)" ");
         header = concat(cast(const char*)header, cast(const char*)"HTTP/1.1\r\n");
+        header = concat(cast(const char*)header, cast(const char*)"Content-Type: text/plain\r\n");
+        header = concat(cast(const char*)header, cast(const char*)"Content-Length: ");
+        header = concat(cast(const char*)header, cast(const char*)contentLength);
+        header = concat(cast(const char*)header, cast(const char*)"\r\n\r\n");
         header = concat(cast(const char*)header, cast(const char*) contentBody);
-        header = concat(cast(const char*)header, cast(const char*)"\r\n");
-
+        header = concat(cast(const char*)header, cast(const char*)"\r\n\r\n");
         socket.Send(cast(byte[])header);
         char[] buffer = (cast (char*)malloc(char.sizeof * 5024))[0..5024];
         socket.Recieve(buffer);
@@ -55,5 +60,9 @@ unittest
 {
     auto s = Http!(string).Get("127.0.0.1", "/", 4000);
     printf(s.Data.ptr);
+    printf("\n\n");
+
+    auto postResult = Http!(string).Post("127.0.0.1", "/", "This is some content", 4000);
+    printf(postResult.Data.ptr);
     printf("\n");
 }
